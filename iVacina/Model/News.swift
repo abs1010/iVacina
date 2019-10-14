c3e45aece2c8c1c15ac7260c6518d927deea03e0
Alan Silva <abs10@globomail.com>
2019-10-14 18:29:59 -0300

    Telas News e Detalhe News Cell no StoryBoard com as respectivas vcs

---
+++ iVacina/Model/News.swift
@@ -0,0 +1,17 @@
+//
+//  News.swift
+//  iVacina
+//
+//  Created by Alan Silva on 14/10/19.
+//  Copyright © 2019 Alan Silva. All rights reserved.
+//
+
+import Foundation
+
+struct news {
+    var mainTopic : String
+    var readingTime : String
+    var descriptiveNews : String
+    var date : Date
+    var image : String
+}
---
+++ iVacina/View/News/NewsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsViewController.swift
+//  iVacina
+//
+//  Created by Alan Silva on 14/10/19.
+//  Copyright © 2019 Alan Silva. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+}
